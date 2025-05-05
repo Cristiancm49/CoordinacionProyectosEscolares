25e952803cbcaebeb56998e890d6da8ddfdbc54f
Cristian <gustavo@MacBook-Pro.local>
2025-05-05 17:11:54 -0500

    se agrega getusuariosbyid para obtener usuarios por id

--- backend/src/controllers/usuarioControllers.js
+++ backend/src/controllers/usuarioControllers.js
@@ -13,5 +13,17 @@ const usuarioController = {
         }
     },
 
-    getUsuariosById: 6
+    getUsuariosById: async (req, res) => {
+        const { id } =  req.params;
+        try {
+            const result = await pool.query(`SELECT * FROM usuario WHERE idusuario = $1`, [id]);
+            if (result.rows.length === 0) {
+                return res.status(404).json({ message: 'Usuario no encontrado.' });
+            }
+            res.json(result.rows[0]);
+        } catch (error) {
+            console.error('Error al obtener usuario por ID:', error);
+            res.status(500).json({ message: 'Error al obtener usuario por ID.' });
+        }
+    }
 }
