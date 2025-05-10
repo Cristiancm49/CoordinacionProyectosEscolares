e654cf396e87efa5e17019dba9b862a6b1587e5b
Cristian <gustavo@MacBook-Pro.local>
2025-05-10 12:42:54 -0500

    se configura la conexion a mongo

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
+const conectMongo = require('./config/mongo');
+conectMongo();
 app.use(cors());
 app.use(express.json());
 
